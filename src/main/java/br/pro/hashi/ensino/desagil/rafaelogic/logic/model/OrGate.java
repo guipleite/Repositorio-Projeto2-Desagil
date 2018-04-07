c4df7d159e28baf0b0c5bec60613251460163b2e
guipleite <guipleite@gmail.com>
2018-04-07 09:26:10 -0300

    estrutura de entrega 2

---
+++ src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/logic/model/OrGate.java
@@ -0,0 +1,37 @@
+package br.pro.hashi.ensino.desagil.rafaelogic.logic.model;
+
+public class OrGate extends Gate{
+
+	private NandGate nandGatea;
+	private NandGate nandGateb;
+	private NandGate nandGatec;
+	
+	public OrGate() {
+		
+		nandGatea = new NandGate(); //Cria 3 nandgate
+		nandGateb = new NandGate();
+		nandGatec = new NandGate();
+		
+		nandGatea.connect(0,nandGateb);
+		nandGatea.connect(1,nandGatec);
+		
+	}
+	
+	@Override
+	public void connect(int pinIndex, Emitter emitter) {
+		if(pinIndex == 0){
+			nandGateb.connect(0, emitter);
+			nandGateb.connect(1, emitter);
+		}
+		if(pinIndex == 1){
+			nandGatec.connect(0, emitter);
+			nandGatec.connect(1, emitter);
+		}
+
+	}
+
+	@Override
+	public boolean read() {
+		return nandGatea.read(); // retorna a saida do ultimo nand
+	}
+}
