b9abfb368964b0f6c09284315b21fe62497da915
Guilherme Leite <guipleite@gmail.com>
2018-04-08 18:58:17 -0300

    view no template do calc

--- src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/logic/view/View.java
+++ src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/logic/view/View.java
@@ -1,5 +1,52 @@
 package br.pro.hashi.ensino.desagil.rafaelogic.logic.view;
 
-public class View {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.LinkedList;
 
+import javax.swing.BoxLayout;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+import br.pro.hashi.ensino.desagil.rafaelogic.logic.view.GateView;
+import br.pro.hashi.ensino.desagil.rafaelogic.logic.model.Gate;
+
+public class View extends JPanel implements ActionListener{
+	
+	private static final long serialVersionUID = 1L;
+
+	private JComboBox<Gate> menu;
+	private GateView gateView;
+	
+	public View(LinkedList<Gate> model){
+		menu = new JComboBox<>();
+		
+		for(Gate gate: model) {
+			menu.addItem(gate);
+		}
+		
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		
+		add(menu);
+		addGateView(0);
+		
+		menu.addActionListener(this);
+	}
+	
+	private void addGateView(int index) {
+		Gate gate = menu.getItemAt(index);
+		gateView = new GateView(gate);
+		add(gateView);
+	}
+	
+	public void actionPerformed(ActionEvent event) {
+		remove(gateView);
+		int index = menu.getSelectedIndex();
+		addGateView(index);
+
+		((JFrame) SwingUtilities.getRoot(this)).pack();
+	}
+	
 }
