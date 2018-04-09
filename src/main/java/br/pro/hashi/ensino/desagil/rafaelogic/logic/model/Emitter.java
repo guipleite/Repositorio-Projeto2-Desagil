8b60bcd522494871def54281577bf9afa41da7bc
guipleite <guipleite@gmail.com>
2018-04-09 15:01:54 -0300

    tentando consertar as classes e etc..

---
+++ src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/logic/model/Emitter.java
@@ -0,0 +1,5 @@
+package br.pro.hashi.ensino.desagil.rafaelogic.logic.model;
+
+public interface Emitter {
+	public boolean read();
+}
---
+++ src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/logic/model/Gate.java
@@ -0,0 +1,4 @@
+package br.pro.hashi.ensino.desagil.rafaelogic.logic.model;
+
+public abstract class Gate implements Emitter, Receiver {
+}
